8f3c9aa0e4eae612dd9a4ec666096e97661fb3e0
Parag Jain <paragjain95@gmail.com>
2020-04-01 11:58:52 +0200

    first version

---
+++ connect-4.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+    return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT -1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+def winning_check():
+    pass
+
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
+
+while not game_over:
+    # Player 1
+    if turn == 0:
+        col = int(input("player 1's turn, choose column 0-6: "))
+        
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+    # Player 2 
+    else:
+        col = int(input("player 2's turn: , choose column 0-6 "))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+    print_board(board)
+    turn += 1
+    turn = turn%2
+
+
+
